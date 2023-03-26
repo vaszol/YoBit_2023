0c5e03ccd73c81130aebbac599a6789bfc865206
vasiliy <vaszol@mail.ru>
2023-03-26 10:34:49 +0300

    добавил info

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import DepthApi from "./components/DepthApi";
+import InfoApi from "./components/InfoApi";
+import {Stack} from "@mui/material";
 
 function App() {
     return (
         <div className="wrapper">
             <Header/>
-            <DepthApi/>
+            <Stack direction="row" spacing={2}>
+                <DepthApi/>
+                <InfoApi/>
+            </Stack>
             <Footer/>
         </div>
     );
--- src/main/frontend/src/components/DepthApi.js
+++ src/main/frontend/src/components/DepthApi.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import axios from "axios";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import {CardActionArea} from '@mui/material';
+import {Button, Card, CardActionArea, CardActions, CardContent, Typography} from "@mui/material";
+import Moment from 'moment';
 
 class DepthApi extends Component {
 
@@ -13,22 +11,32 @@ class DepthApi extends Component {
             pair: "aaa_bbb",
             ask: "0.0...",
             bid: "0.0...",
-            date: "..."
-        }
+            date: ""
+        },
+        loaded: false
     }
 
     getDepth = () => {
         axios.get('http://localhost:8080/api/getDepth?pair=usd_rur')
             .then(({data}) => {
-                console.log(data);
                 this.setState({data: data})
             });
         return undefined;
     }
 
+    updateDepth = () => {
+        this.setState({loaded: true})
+        axios.get('http://localhost:8080/api/updateDepth?pair=usd_rur')
+            .then(({data}) => {
+                this.setState({data: data, loaded: false})
+            });
+        return undefined;
+    }
+
     render() {
-        const {data} = this.state;
-        console.log(data.date);
+        const {data, loaded} = this.state;
+        const date = data.date ? Moment(data.date).format('hh:mm:ss D-MMM-YY') : '...'
+
         return (
             <div>
                 <Card sx={{maxWidth: 200}} onClick={() => this.getDepth()}>
@@ -38,10 +46,14 @@ class DepthApi extends Component {
                             <Typography variant="body2" color="text.secondary">Depth</Typography>
                             <Typography variant="body2" color="text.secondary">{data.ask}</Typography>
                             <Typography variant="body2" color="text.secondary">{data.bid}</Typography>
-                            <Typography variant="body2" color="text.secondary">{data.date}</Typography>
+                            <Typography variant="body2" color="text.secondary">{date}</Typography>
                         </CardContent>
                     </CardActionArea>
                 </Card>
+                <Button sx={{maxWidth: 200}} variant="outlined" color="error" onClick={() => this.updateDepth()}
+                        disabled={loaded}>
+                    ! Update Depth DB !
+                </Button>
             </div>
         );
     }
